9d03b1f6fc92653dcce5c352be2904ce33ab9e58
Stefan Meinecke <meinecke@greensec.de>
2023-09-17 00:14:30 +0200

    Update add-repository.sh
    Add missing software-properties-common package.

--- add-repository.sh
+++ add-repository.sh
@@ -6,7 +6,7 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 ${SUDO} apt-get update
-${SUDO} apt-get -y install lsb-release ca-certificates wget
+${SUDO} apt-get -y install software-properties-common lsb-release ca-certificates wget
 ${SUDO} wget -O /usr/share/keyrings/greensec.github.io-bacula-fork.key https://greensec.github.io/bacula-fork-deb-build/public.key
 ${SUDO} apt-add-repository "deb [signed-by=/usr/share/keyrings/greensec.github.io-bacula-fork.key arch=amd64] https://greensec.github.io/bacula-fork-deb-build/repo $(lsb_release -sc) main"
 ${SUDO} apt-get update
