29c5e18f0efd758133b148d3361237b431ada4e4
Stefan <meinecke@greensec.de>
2024-11-29 04:39:54 +0100

    add multiarch.

--- scripts/build.sh
+++ scripts/build.sh
@@ -55,6 +55,6 @@ if [ -d "../patches.$DEB_FLAVOR" ]; then
   cp -va ../patches.$DEB_FLAVOR debian/
 fi
 
-DEB_BUILD_OPTIONS="noautodbgsym nocheck nodocs" dpkg-buildpackage -j$(nproc) -d -us -b
+CONFIG_SITE="/etc/dpkg-cross/cross-config.$DEB_HOST_ARCH" DEB_BUILD_OPTIONS="noautodbgsym nocheck nodocs" dpkg-buildpackage -j$(nproc) -d -us -b
 cd ..
 rm -vf *-dbg*.deb
